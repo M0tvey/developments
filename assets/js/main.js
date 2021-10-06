1af7a122809e9fefd084387b8a86e8a018b645a7
Moroz Kostua <moroz.k@avd-company.ru>
2021-10-06 17:29:36 +0300

    Update 2021-10-06T14:29:33.988Z

--- assets/js/main.js
+++ assets/js/main.js
@@ -1089,74 +1089,6 @@ class coreMap {
     setTimeout(_ => { this.toggleIconAndClass(icon) }, 600);
   }
 };
-// -------------------------------- verticla adaptive accardion --------------------------------
-(_=>{
-  const verAccords = $('.js_ver_accord'),
-    openClass = 'ver_accord_open',
-    breakpoints = {
-      'xlll': 1750,
-      'xll': 1400,
-      'xl': 1200,
-      'lg': 992,
-      'md': 768,
-      'sm': 576,
-      'xs': 320
-    };
-
-  verAccords.each(function () {
-    const verAccord = $(this),
-      verAccordLinkClass = 'js_ver_accord_link'
-      verAccordLinks = verAccord.find(`.${ verAccordLinkClass }`),
-      verAccordBlockClass = 'js_ver_accord_block',
-      verAccordItemClass = 'js_ver_accord_item',
-      linkInit = _=> {
-        verAccordLinks.each(function () {
-          const $link = $(this),
-            verAccordBlock = $link.siblings(`.${ verAccordBlockClass }`)
-
-          if (!$link.parent(`.${ verAccordItemClass }`).is(`.${ openClass }`)) verAccordBlock.hide();
-
-          $link.on('click', function (e) {
-            e.preventDefault();
-
-            verAccord.find(`.${ verAccordItemClass }.${ openClass }`).each(function(){
-              $(this).removeClass(openClass);
-              $(this).find(`.${ verAccordBlockClass }`).slideUp();
-            });
-
-            if (verAccordBlock.is(':hidden')) {
-              verAccordBlock.closest(`.${ verAccordItemClass }`).addClass(openClass);
-              verAccordBlock.slideDown();
-            }
-          });
-        });
-      };
-      
-    if (verAccord[0].className.indexOf('js_accard-')) {
-      const pointName = verAccord.attr('class').split('js_accard-').pop();
-      let isActive = innerWidth < breakpoints[pointName];
-      
-      if (isActive) linkInit();
-      
-      $(window).on('resize', function () {
-        isActive = innerWidth < breakpoints[pointName];
-
-        if (isActive) {
-          if (!testEvent(verAccordLinks[0], 'click')) linkInit();
-        } else {
-          verAccord.find(`.${ verAccordItemClass }`).each(function(){
-            const $item = $(this);
-            $item.show().removeClass(openClass);
-            $item.find(`.${ verAccordLinkClass }`).off('click');
-            $item.find(`.${verAccordBlockClass}`).slideDown();
-          });
-        }
-      });
-    } else {
-      linkInit();
-    }
-  });
-})();
 // ------------------------------------ start custom cursor ------------------------------------
 (function () {
   setTimeout(() => {
@@ -1482,6 +1414,14 @@ const breakpoints = {
   'md': 768,
   'sm': 576,
   'xs': 320
+};
+
+// ------- check for event -----------
+function testEvent(elem, event, fun) {
+  var events = $._data(elem, "events");
+  return !!events && !!(events = events[event]) && (!fun || events.some(function (data) {
+    return data.handler == fun
+  }));
 };
 
 (function () {
@@ -1942,7 +1882,6 @@ $(document).ready(function () {
           $textBlock.css('height', 'auto');
         }
       } else {
-console.log($(this).height(), minHeight)
         if ($(this).height() < minHeight) {
           $bttn.hide();
           $textBlock.removeClass(hideClass);
@@ -1978,60 +1917,65 @@ console.log($(this).height(), minHeight)
 
   // -------------------------------- verticla adaptive accardion --------------------------------
   (_ => {
-    const verAccords = $('.js_ver_accord');
-
-    verAccords.each(function () {
-      const verAccord = $(this),
-        verAccordLinkClass = 'js_ver_accord_link'
-      verAccordLinks = verAccord.find(`.${verAccordLinkClass}`),
-        verAccordBlockClass = 'js_ver_accord_block',
-        verAccordItemClass = 'js_ver_accord_item',
-        linkInit = _ => {
-          verAccordLinks.each(function () {
-            const $link = $(this),
-              verAccordBlock = $link.siblings(`.${verAccordBlockClass}`)
-
-            if (!$link.parent(`.${verAccordItemClass}`).is(`.${openClass}`)) verAccordBlock.hide();
-
-            $link.on('click', function (e) {
-              e.preventDefault();
-
-              verAccord.find(`.${verAccordItemClass}.${openClass}`).each(function () {
-                $(this).removeClass(openClass);
-                $(this).find(`.${verAccordBlockClass}`).slideUp();
+    verAccordion = _ => {
+      const verAccords = $('.js_ver_accord'),
+        openClass = 'ver_accord_open';
+  
+      verAccords.each(function () {
+        const verAccord = $(this),
+          pointName = verAccord.data('active'),
+          verAccordLinkClass = 'js_ver_accord_link',
+          verAccordLinks = verAccord.find(`.${verAccordLinkClass}`),
+          verAccordBlockClass = 'js_ver_accord_block',
+          verAccordItemClass = 'js_ver_accord_item',
+          linkInit = _ => {
+            verAccordLinks.each(function () {
+              const $link = $(this),
+                verAccordBlock = $link.siblings(`.${verAccordBlockClass}`)
+  
+              if (!$link.parent(`.${verAccordItemClass}`).is(`.${openClass}`)) verAccordBlock.hide();
+  
+              $link.on('click', function (e) {
+                e.preventDefault();
+  
+                verAccord.find(`.${verAccordItemClass}.${openClass}`).each(function () {
+                  $(this).removeClass(openClass);
+                  $(this).find(`.${verAccordBlockClass}`).slideUp();
+                });
+  
+                if (verAccordBlock.is(':hidden')) {
+                  verAccordBlock.closest(`.${verAccordItemClass}`).addClass(openClass);
+                  // verAccordBlock.slideDown();
+                }
               });
-
-              if (verAccordBlock.is(':hidden')) {
-                verAccordBlock.closest(`.${verAccordItemClass}`).addClass(openClass);
-                verAccordBlock.slideDown();
-              }
             });
+          };
+  
+        if (pointName) {
+          let isActive = window.innerWidth < breakpoints[pointName];
+
+          if (isActive) linkInit();
+          $(window).on('resize', function () {
+            isActive = window.innerWidth < breakpoints[pointName];
+  
+
+            if (isActive) {
+  console.log(isActive)
+              if (!testEvent(verAccordLinks[0], 'click')) linkInit();
+            } else {
+              verAccord.find(`.${verAccordItemClass}`).each(function () {
+                // const $item = $(this);
+                // $item.show().removeClass(openClass);
+                // $item.find(`.${verAccordLinkClass}`).off('click');
+                // // $item.find(`.${verAccordBlockClass}`).slideDown();
+              });
+            }
           });
-        };
-
-      if (verAccord[0].className.indexOf('js_accard-')) {
-        const pointName = verAccord.attr('class').split('js_accard-').pop();
-        let isActive = innerWidth < breakpoints[pointName];
-
-        if (isActive) linkInit();
-
-        $(window).on('resize', function () {
-          isActive = innerWidth < breakpoints[pointName];
-
-          if (isActive) {
-            if (!testEvent(verAccordLinks[0], 'click')) linkInit();
-          } else {
-            verAccord.find(`.${verAccordItemClass}`).each(function () {
-              const $item = $(this);
-              $item.show().removeClass(openClass);
-              $item.find(`.${verAccordLinkClass}`).off('click');
-              $item.find(`.${verAccordBlockClass}`).slideDown();
-            });
-          }
-        });
-      } else {
-        linkInit();
-      }
-    });
+        } else {
+          linkInit();
+        }
+      });
+    };
+    verAccordion();
   })();
 });
